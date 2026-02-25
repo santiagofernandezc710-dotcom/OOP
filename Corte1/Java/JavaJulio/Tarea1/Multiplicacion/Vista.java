f93638395d6d00ca87d2d7c878da452d746b9a09
santiagofernandezc710-dotcom <santiagofernandezc710@gmail.com>
2026-02-24 21:17:34 -0500

    Add files via upload

---
+++ Corte1/Java/JavaJulio/Tarea1/Multiplicacion/Vista.java
@@ -0,0 +1,20 @@
+public class Vista 
+{
+
+    public Vista() 
+    {}
+    public void mostrarTitulo()
+    {
+        System.out.println("programa para multiplicar dos numeros");
+    }
+    public void mostrarNumero()
+        {
+            System.out.println("digite numero");
+        }
+        public void mostrarSalida(Numero multiplica)
+        {
+            System.out.println(multiplica.getNumero());
+        }
+
+    
+}
