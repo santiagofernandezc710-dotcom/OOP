bcf2836856f9705fb85c4aa42ed16f845fb0684b
santiagofernandezc710-dotcom <santiagofernandezc710@gmail.com>
2026-02-24 21:13:23 -0500

    Add files via upload

---
+++ Corte1/Java/JavaJulio/Tarea1/Division/Vista.java
@@ -0,0 +1,20 @@
+public class Vista 
+{
+
+    public Vista() 
+    {}
+    public void mostrarTitulo()
+    {
+        System.out.println("programa para dividir dos numeros");
+    }
+    public void mostrarNumero()
+        {
+            System.out.println("digite numero");
+        }
+        public void mostrarSalida(Numero division)
+        {
+            System.out.println(division.getNumero());
+        }
+
+    
+}
